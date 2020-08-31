22961fa3979111fbfd2a2eedd6c646328a40487d
Carl Ching <benedick.ching26@gmail.com>
2020-08-31 19:10:19 +0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  theme: {
+    textColor: {
+      primary: "#03ceac",
+      secondary: "#ffed4a",
+      danger: "#e3342f",
+      white: "white",
+    },
+    fontFamily: {
+      display: ["Comfortaa"],
+    },
+    extend: {},
+  },
+  variants: {
+    flex: ["responsive", "hover", "focus"],
+    alignContent: ["responsive", "hover", "focus"],
+  },
+  plugins: [],
+};
